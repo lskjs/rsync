bca073b7eacc5f17c2dbe17ef1d17c9a4501a653
Igor Suvorov <hi@isuvorov.com>
2020-09-04 12:07:53 +0300

    feat: add all docker tags

--- build/build.sh
+++ build/build.sh
@@ -11,6 +11,15 @@ SCRIPT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t lskjs/rsync:3.1 "$SCRIPT_ROOT/../3.1"
 docker build -t lskjs/rsync:3.6 "$SCRIPT_ROOT/../3.6"
 docker build -t lskjs/rsync:3.9 "$SCRIPT_ROOT/../3.9"
+docker build -t lskjs/rsync:3.10 "$SCRIPT_ROOT/../3.10"
+docker build -t lskjs/rsync:3.11 "$SCRIPT_ROOT/../3.11"
 docker build -t lskjs/rsync:latest "$SCRIPT_ROOT/.."
 
 docker rmi -f $(docker images -q --filter "dangling=true")
+
+docker push lskjs/rsync:3.1
+docker push lskjs/rsync:3.6
+docker push lskjs/rsync:3.9
+docker push lskjs/rsync:3.10
+docker push lskjs/rsync:3.11
+docker push lskjs/rsync:latest
